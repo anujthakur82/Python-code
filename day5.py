c29b670878c554f7c9919f8a8d1df09f794aec42
anujthakur82 <singhalok.an7800@gmail.com>
2025-03-16 00:35:41 +0530

    c

---
+++ day5.py
@@ -0,0 +1,8 @@
+#Learning Operators
+print(15+6)
+print(15-6)
+print(15*6)
+print(15/6) #(division)Decimal value of quotient is printed in this
+print(15//6) #This is known as 'floor division'.Decimal value of quotient is not printed in this
+print(15%6) #(Modulus)This only print a remainder
+print(5**3) #(Exponential)This multiply 5 three times
