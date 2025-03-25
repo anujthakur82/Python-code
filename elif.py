59e495c1d361783924ad78fd6fa66cc261b1a8a2
anujthakur82 <singhalok.an7800@gmail.com>
2025-03-26 01:37:44 +0530

    c

---
+++ elif.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the value: "))
+if (num < 0):
+    print("Number is negative.")
+elif (num == 0):
+    print("Number is Zero.")
+elif(num == 111):
+    print("Number is Special.")
+else:
+    print("Number is postive.")
