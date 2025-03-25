0a934d2069237610678ff88dfc3e6739ee6497ee
anujthakur82 <singhalok.an7800@gmail.com>
2025-03-26 00:53:30 +0530

    c

---
+++ day11.py
@@ -0,0 +1,12 @@
+#if-else statement
+a= int(input("Enter your age: "))
+print("Your age is:", a)
+#conditionl operators: >,<,>=,<=,==,!=
+print(a>18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+if(a>18):
+    print("You are elegible for voting")
+else:
+    print("You are not elegible for voting")
