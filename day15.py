2580a0a4f0a3edc08f4dc87a7a1e91315584d4a8
anujthakur82 <singhalok.an7800@gmail.com>
2025-04-15 18:57:46 +0530

    c

---
+++ day15.py
@@ -0,0 +1,20 @@
+#while loop
+i=0
+while(i<3):
+    print(i)
+    i = i + 1
+print("Done with the loop")
+
+i = int(input("Enter the number:"))
+while(i<=40):
+    i = int(input("Enter the number:"))
+    print(i)
+print("Done with the loop")
+
+#decrementing while loop
+count = 5
+while(count > 0):
+    print(count)
+    count = count -1
+else:
+    print("I am inside else")  
