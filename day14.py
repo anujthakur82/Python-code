0ddc96d4faa7844476d46adb8746f47cbde6b9db
anujthakur82 <singhalok.an7800@gmail.com>
2025-04-15 17:33:37 +0530

    c

---
+++ day14.py
@@ -0,0 +1,27 @@
+#for loop 
+name = 'Alok'
+for i in name:
+  print(i)
+  if(i=="A"):
+    print("This is special!")
+
+colors=["Red","Green","Blue","Yellow"]
+for color in colors:
+  print(color)
+  for i in color:
+    print(i)    
+
+for k in range(5):
+  print(k)
+
+for k in range(5):
+  print(k+1)
+
+for k in range(1,9):
+  print(k)    
+
+for i in range(0, 10, 2):
+    print(i)
+
+for i in range(10,0,-2):
+  print(i)
